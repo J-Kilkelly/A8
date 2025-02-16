0d4e3b6ebb1c2a3ba1f5a36188ed149e9179d40e
John Kilkelly <JKilkelly@live.com>
2025-02-16 01:39:43 -0500

    if-else statement (index > 0)

--- Program.cs
+++ Program.cs
@@ -8,9 +8,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<string> terms = new List<string> (File.ReadAllLines(@"C:\Users\jkilk\OneDrive\SPC\CSharp 1 2360\A8\horticulture_terms.txt"));
-        terms.Sort();
-        Console.WriteLine($"\n{terms[0]}");
-       
+        Console.WriteLine($"\nEnter search term.");
+        string? searchTerm = Console.ReadLine();
+        BinarySearch mySearch = new BinarySearch();
+        int myIndex = mySearch.SearchList(searchTerm);
+        
+        if (myIndex>0)
+            Console.WriteLine($"The term '{searchTerm}' was found at index {myIndex.ToString()}.");
+        else
+            Console.WriteLine($"The term '{searchTerm}' was not found.");
     }
+
+    public class BinarySearch
+    {
+        List<string> terms = new List<string> (File.ReadAllLines(@"C:\Users\jkilk\OneDrive\SPC\CSharp 1 2360\A8\horticulture_terms.txt"));
+        
+       public int SearchList(string searchTerm)
+        {
+            terms.Sort();
+            int index = terms.BinarySearch(searchTerm);
+            return index;
+        }
+    }
+  
 }
