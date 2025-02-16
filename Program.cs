af8bea7868daa58068d678a5a5705679548cdf66
John Kilkelly <JKilkelly@live.com>
2025-02-16 13:08:33 -0500

    made searchTerm and main list of terms all lowercase

--- Program.cs
+++ Program.cs
@@ -10,9 +10,11 @@ class Program
     {
         Console.WriteLine($"\nEnter search term.");
         string? searchTerm = Console.ReadLine();
-        BinarySearch mySearch = new BinarySearch();
-        int myIndex = mySearch.SearchList(searchTerm);
-        
+        BinarySearch mySearch = new BinarySearch(); //create instance of BinarySearch class
+
+        int myIndex = mySearch.SearchList(searchTerm.ToLower());
+        //binary search is case sensative. Make searchTerm all lowercase. SearchList method will make all terms in main list lowercase.
+
         if (myIndex>0)
             Console.WriteLine($"The term '{searchTerm}' was found at index {myIndex.ToString()}.");
         else
@@ -21,10 +23,16 @@ class Program
 
     public class BinarySearch
     {
-        List<string> terms = new List<string> (File.ReadAllLines(@"C:\Users\jkilk\OneDrive\SPC\CSharp 1 2360\A8\horticulture_terms.txt"));
-        
+        static string filePath = @"C:\Users\jkilk\OneDrive\SPC\CSharp 1 2360\A8\horticulture_terms.txt";
+        List<string> terms = new List<string> (File.ReadAllLines(filePath));
+       
        public int SearchList(string searchTerm)
         {
+            //make all terms in list lowercase to match the (lower) case of the search term
+            for (int i =0; i < terms.Count; i++)
+                {
+                    terms[i] = terms[i].ToLower();
+                }
             terms.Sort();
             int index = terms.BinarySearch(searchTerm);
             return index;
