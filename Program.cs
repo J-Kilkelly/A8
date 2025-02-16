0ccdb93ca464d76cce240d44c5df7da0fb3b42e4
John Kilkelly <JKilkelly@live.com>
2025-02-15 22:50:57 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace KilkellyJA8;
+//John Kilkelly
+//ID: 350439
+//COP2360
+//Assignment 8
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> terms = new List<string> (File.ReadAllLines(@"C:\Users\jkilk\OneDrive\SPC\CSharp 1 2360\A8\horticulture_terms.txt"));
+        terms.Sort();
+        Console.WriteLine($"\n{terms[0]}");
+       
+    }
+}
